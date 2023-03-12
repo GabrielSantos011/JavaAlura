5d5623aca3ad450fdb1ddd7477dabf1db8e7560d
Gabriel <gabrielrafael.grs@gmail.com>
2023-03-12 15:09:59 -0300

    Object

---
+++ 5-JavaLang/src/br/com/empresa/nomeProjeto/parteTres/TestaObject.java
@@ -0,0 +1,9 @@
+package br.com.empresa.nomeProjeto.parteTres;
+
+public class TestaObject {
+    /*
+    * todas as classes herdam de object automaticamente
+    * classe mais genérica de todas para polimorfismo
+    * boa prática reescrever o método toString() e o equals()
+    */
+}
