b3bf6e5adf9223511949260d1699182dde235316
Gabriel <gabrielrafael.grs@gmail.com>
2023-03-13 01:47:42 -0300

    enums

---
+++ Extra1-Enums/src/Prioridade.java
@@ -0,0 +1,26 @@
+public enum Prioridade {
+    //enums são constantes
+    //funcionam como uma classe normal
+    //porém funcionam de uma maneira diferente
+    //cada constante aqui é como se fosse um objeto
+    //e de uma maneira básica para criar basta:
+    //BAIXA, MEDIA, ALTA
+
+    //porém se quisermos alguns valores customizados podemos fazer dessa maneira
+    //para isso precisaremos de um construtor (esse construtor é privado)
+    BAIXA(1),
+    MEDIA(5),
+    ALTA(10);
+
+    private int valor;
+
+    Prioridade(int valor) {
+        this.valor = valor;
+    }
+
+    //podemos ter métodos nas enums
+    public int getValor() {
+        return valor;
+    }
+
+}
