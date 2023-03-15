a77aef4e6999d5087d76805dd7ebfbeaf87b7076
Gabriel <67613303+GabrielSantos011@users.noreply.github.com>
2023-03-15 16:58:15 -0300

    equals

---
+++ 6-ColecoesWrappersLambdaExpressions/src/quartaAula/Conta.java
@@ -0,0 +1,42 @@
+package quartaAula;
+
+public class Conta
+{
+    
+    private int agencia;
+    private int conta;
+    
+    public Conta(int agencia, int conta) {
+        this.agencia = agencia;
+        this.conta = conta;
+    }
+    
+    public int getAgencia( )
+    {
+        return agencia;
+    }
+    
+    public int getConta( )
+    {
+        return conta;
+    }
+    
+    //sobrescrevendo o método equals
+    //método utilizado para comparar os objetos
+    //o padrão vem de Object - compara somente as referencias
+    //alterando para que caso a agencia e conta sejam iguais retorne true
+    //ou seja, podemos ter dois objetos distintos no meio do código
+    //porém caso eles se enquadrem nessa condição estamos 
+    //falando da mesma conta!
+    @Override
+    public boolean equals( Object obj )
+    {
+        Conta c = (Conta) obj;
+        
+        if( !(this.conta == c.getConta()) && !(this.agencia == c.getAgencia()) )
+            return false;
+
+        return true;
+    }
+    
+}
---
+++ 6-ColecoesWrappersLambdaExpressions/src/quartaAula/TestaEquals.java
@@ -0,0 +1,25 @@
+package quartaAula;
+
+import java.util.ArrayList;
+
+public class TestaEquals
+{
+    
+    public static void main( String[] args )
+    {
+        ArrayList<Conta> lista = new ArrayList<>();
+        
+        lista.add( new Conta(11, 11) );
+        lista.add( new Conta(22, 22) );
+        lista.add( new Conta(33, 33) );
+        
+        //temos o método contains para perguntar se a 
+        //lista contém determinado parametro
+        //internamente este método chama o equals 
+        //(sobrescrito na classe Conta)
+        System.out.println( lista.contains( new Conta(22, 22) ) );
+        //caso não houvesse a sobrescrita o método compararia as referencias
+        //e no caso são duas referencias diferentes retornando assim false
+    }
+    
+}
