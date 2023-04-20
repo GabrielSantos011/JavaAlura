965408df77685ac8ab9b090f5d36821280fe71d0
Gabriel <gabrielrafael.grs@gmail.com>
2023-04-20 18:44:59 -0300

    encoding

---
+++ 7-JavaIO/src/quintaAula/TesteEncodingLeituraEEscrita.java
@@ -0,0 +1,21 @@
+package quintaAula;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class TesteEncodingLeituraEEscrita {
+
+    public static void main(String[] args) throws IOException {
+        InputStream fis = new FileInputStream("lorem.txt");
+        Reader      isr = new InputStreamReader(fis, "UTF-8");
+
+        OutputStream fos = new FileOutputStream("a.txt");
+        Writer       osw = new OutputStreamWriter(fos, "UTF-8");
+
+        FileReader fr = new FileReader("lorem.tx", StandardCharsets.UTF_8);
+
+        //assim por diante
+        
+    }
+
+}
---
+++ 7-JavaIO/src/quintaAula/TesteUnicodeEEncoding.java
@@ -0,0 +1,39 @@
+package quintaAula;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class TesteUnicodeEEncoding {
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String s = "ç";
+        //codepoint no Unicode
+        System.out.println(s.codePointAt(0));
+
+        //qual o encoding padrão
+        Charset charset = Charset.defaultCharset();
+        System.out.println(charset.displayName());
+
+        //encoding das strings usando o parametro (sem parametro usa o padrão)
+        byte[] bytes = s.getBytes("windows-1252");
+        System.out.print(bytes.length + ", windows-1252, ");
+        //nova string baseada em um fluxo de bytes com encoding passado via parametro
+        //(caso não seja passado utiliza o padrão)
+        String sNovo = new String(bytes, "windows-1252");
+        System.out.println(sNovo);
+        //caso o enconding desejado fosse diferente do fluxo de bytes passados
+        //poderíamos ter problemas de encoding
+
+        bytes = s.getBytes("UTF-16");
+        System.out.print(bytes.length + ", UTF-16, ");
+        sNovo = new String(bytes, "UTF-16");
+        System.out.println(sNovo);
+
+        bytes = s.getBytes(StandardCharsets.US_ASCII);
+        System.out.print(bytes.length + ", US-ASCII, ");
+        sNovo = new String(bytes, "US-ASCII");
+        System.out.println(sNovo);
+
+    }
+}
