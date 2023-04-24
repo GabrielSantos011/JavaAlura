110ff85a9d03c900d58f66933ee22493faa4761d
Gabriel <gabrielrafael.grs@gmail.com>
2023-04-24 17:14:15 -0300

    observação na geração/ leitura de arquivos

--- 7-JavaIO/src/br/com/alura/java/io/segundaAula/parteUm/TesteEscrita.java
+++ 7-JavaIO/src/br/com/alura/java/io/segundaAula/parteUm/TesteEscrita.java
@@ -9,6 +9,8 @@ public class TesteEscrita {
         //porém com clsses diferentes
         //OutputStream e Writer
         OutputStream fos = new FileOutputStream("arquivo-segunda-aula-parte1.txt");
+        //obs: caso querira salvar em um diretório do Pc passamos junto o path
+        //exemplo C:\\teste.txt (para funcionar na raiz C a IDE tem que estar executando como adm)
         Writer osw = new OutputStreamWriter(fos);
         BufferedWriter bw = new BufferedWriter(osw);
 
